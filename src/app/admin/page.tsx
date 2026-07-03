44ed64485a99735a9888fc038d85c17344ed1f6a
hpintojr <hpintojr@gmail.com>
2026-07-02 20:07:32 -0700

    chore(debug): trace protected admin route entry

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,1 +1,6 @@
-export { default } from "./page-v3";
+import AdminPageV3 from "./page-v3";
+
+export default async function AdminPage(props: Parameters<typeof AdminPageV3>[0]) {
+  console.info("[route-trace] admin page entered");
+  return <AdminPageV3 {...props} />;
+}
