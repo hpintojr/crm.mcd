2604376b6f2e316addf7da97a7f307f0490bbcf6
hpintojr <hpintojr@gmail.com>
2026-07-12 09:32:23 -0700

    Gate admin page route trace

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,7 @@
+import { routeTrace } from "@/lib/route-trace";
 import AdminPageV3 from "./page-v3";
 
 export default async function AdminPage(props: Parameters<typeof AdminPageV3>[0]) {
-  console.info("[route-trace] admin page entered");
+  routeTrace("admin page entered");
   return <AdminPageV3 {...props} />;
 }
